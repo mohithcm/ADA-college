5754230e7b9e7c665aa8864e89e34d4356a0b783
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-08-10 14:00:02 +0530

    Bubble, Selection and Insertion sort analysis

--- Problem 3/bubble.c
+++ Problem 3/bubble.c
@@ -2,18 +2,19 @@
 #include<stdlib.h>
 
 int sort(int arr[], int n){
-    int flag = 1, count = 0;
+    int count = 0, flag;
     for(int i = 0; i<n-1; ++i){
-        for(int j = 0; j<n-1-j; ++j){
-            if ( arr[j] > arr[j+1] ){
-                ++count;
-                int t = arr[j+1];
+        flag = 1;
+        for(int j = 0; j<n-1-i; ++j){
+            ++count;
+            if ( arr[j+1] < arr[j] ){
+                int temp = arr[j+1];
                 arr[j+1] = arr[j];
-                arr[j] = t;
+                arr[j] = temp;
                 flag = 0;
             }
         }
-        if (flag == 1)
+        if (flag)
             return count;
     }
     return count;
@@ -27,7 +28,7 @@ void main(){
     b = fopen("best.txt", "a");
     w = fopen("worst.txt", "a");
 
-    for(int n = 0; n<=100; n+=10){
+    for(int n = 10; n<=100; n+=10){
         int * arr = (int *)calloc(sizeof(int), n);
 
         // best case
@@ -39,7 +40,7 @@ void main(){
         fprintf(w, "%d  %d\n", n, sort(arr, n));
 
         // avg case
-        for (int i = 0; i<n; ++i)
+        for (int i = 0; i<n/2; ++i)
             arr[i] = rand()%100;
         fprintf(a, "%d  %d\n", n, sort(arr, n));    
     }
